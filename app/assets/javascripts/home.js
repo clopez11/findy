9291d5eed0d19e1328cf882a342e060f5816b12b
prateeksan <sanyal.prateek@gmail.com>
2016-02-09 20:19:59 -0500

    Can display stuff from jquery now

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-  $.getJSON('/contacts.json', function(data){
-    console.log(data);
-  });
+  $('.contact_info').hide()
+  $('#index').on('click', function(){
+    $('.contact_info').show()
+    $.getJSON('/contacts.json', function(data){
+      $('#email').html(data[0].email);
+      $('#first_name').html(data[0].first_name);
+      $('#last_name').html(data[0].last_name);
+      $('#phone_number').html(data[0].phone_number);
+    });   
+  })
+
 });
