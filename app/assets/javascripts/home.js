d0674972393c3336e3a86c35a149df7f53d4b649
prateeksan <sanyal.prateek@gmail.com>
2016-02-10 13:03:23 -0500

    New contact functionality ready

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -13,6 +13,38 @@ $(document).ready(function(){
     $('#hide_index').hide();
     $('#index').show();
   });
+  $("#new").on('click', function(){
+    hide_contacts();
+    var open_ul = "<ul>"
+    var email = "<li> Email: <input type='text' id='email'></li>";
+    var first_name = "<li> First Name: <input type='text' id='first_name'></li>";
+    var last_name = "<li> Last Name: <input type='text' id='last_name'></li>";
+    var phone_number = "<li> Phone Number: <input type='number' id='phone_number'></li>";
+    var update_and_delete = "<li><button id='submit_new_contact'>Add</button></li>";
+    var close_ul = "</ul>"
+    // contact.first_name = 
+    $('.home').append(open_ul, email, first_name, last_name, phone_number, update_and_delete, close_ul);
+    $("#submit_new_contact").on('click', function(){
+      var contact = {};
+      contact.email = $("#email").val();
+      contact.first_name = $("#first_name").val();
+      contact.last_name = $("#last_name").val();
+      contact.phone_number = $("#phone_number").val();
+      contact = {contact: contact}
+      $.ajax({
+        url: '/contacts.json',
+        type: 'POST',
+        data: contact,
+        success: function(result) {
+          console.log('created contact successfully');
+          hide_contacts();
+          $.getJSON('/contacts.json', function(data){
+            list_contacts(data);
+          });
+        }
+      });
+    });
+  });
 
   $("#contact_id").on('keyup', function(){
     hide_contacts();
@@ -50,9 +82,9 @@ $(document).ready(function(){
       var first_name = "<li> First Name: " + data[i].first_name + "</li>";
       var last_name = "<li> Last Name: " + data[i].last_name + "</li>";
       var phone_number = "<li> Phone Number: " + data[i].phone_number + "</li>";
-      var delete_contact = "<li><button class='delete_contact_button' data-contact-id=" + i + ">Delete</button></li>";
+      var update_and_delete = "<li><button class='update_contact_button' data-contact-id=" + i + ">Update</button> <button class='delete_contact_button' data-contact-id=" + i + ">Delete</button></li>";
       var close_ul = "</ul>"
-      $('.home').append(open_ul, first_name, last_name, phone_number, email, delete_contact, close_ul)
+      $('.home').append(open_ul, first_name, last_name, phone_number, email, update_and_delete, close_ul)
       $('.delete_contact_button').on('click', function(){
         var contact_pos = this.getAttribute("data-contact-id");
         $.ajax({
@@ -66,9 +98,24 @@ $(document).ready(function(){
             });
           }
         });
-      })
-    }
+      });
+      $('.update_contact_button').on('click', function(){
+        var contact_pos = this.getAttribute("data-contact-id");
+        $.ajax({
+          url: '/contacts/' + data[contact_pos].id + '.json',
+          type: 'PATCH',
+          success: function(result) {
+            console.log('update successfully');
+            hide_contacts();
+            $.getJSON('/contacts.json', function(data){
+              list_contacts(data);
+            });
+          }
+        });
+      });
+    };
   };
+
   function hide_contacts(){
     $("li").remove();
   };
