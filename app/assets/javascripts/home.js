4cc9a152bb0efbb8cb7bc8da45697c785dcd741a
prateeksan <sanyal.prateek@gmail.com>
2016-02-09 21:06:20 -0500

    Show/hide functionality working

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
-  $('.contact_info').hide()
+  $('#hide_index').hide();
   $('#index').on('click', function(){
-    $('.contact_info').show()
+    $('#hide_index').show();
+    $('#index').hide();
     $.getJSON('/contacts.json', function(data){
-      $('#email').html(data[0].email);
-      $('#first_name').html(data[0].first_name);
-      $('#last_name').html(data[0].last_name);
-      $('#phone_number').html(data[0].phone_number);
-    });   
-  })
+      for (i = 0; i < data.length; i++){
+        var open_ul = "<ul>"
+        var email = "<li> Email: " + data[i].email + "</li>";
+        var first_name = "<li> First Name: " + data[i].first_name + "</li>";
+        var last_name = "<li> Last Name: " + data[i].last_name + "</li>";
+        var phone_number = "<li> Phone Number: " + data[i].phone_number + "</li>";
+        var close_ul = "</ul>"
+        $('.home').append(open_ul, first_name, last_name, phone_number, email, close_ul)
+      }
+    });
+  });
+  $("#hide_index").on('click', function(){
+    $("li").remove();
+    $('#hide_index').hide();
+    $('#index').show();
+  });
+  
 
 });
